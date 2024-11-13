2361b24b83f3a1349861c68a7b93a887c1f82496
Rami-CUD <20220001825@students.cud.ac.ae>
2024-11-13 12:22:28 +0400

    Added multiprocessing capabilities, with mock categorization (trivial approach using only file name)

--- organizer.py
+++ organizer.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from sys import argv
-from os import makedirs
+from os import makedirs, listdir, rename
 from enum import StrEnum
+import multiprocessing as mp
 
 class Categories(StrEnum):
     Programming = "Programming"
@@ -12,8 +13,24 @@ class Categories(StrEnum):
 
 def create_dirs(working_dir:Path):
     for category in Categories:
-        makedirs(working_dir.joinpath(category))
+        makedirs(working_dir.joinpath(category), exist_ok=True)
 
+def mock_task(file: Path):
+    for catergory in Categories:
+        if file.stem.lower() == catergory.lower():
+            file.rename(file.parent.joinpath(catergory).joinpath(file.name))
+
+def assign_processes(files: list[Path]):
+    processes: list[mp.Process] = []
+    for file in files:
+        process = mp.Process(target=mock_task, args=(file, ))
+        process.start()
+        processes.append(process)
+    return processes
+        
+def join_processes(processes:list[mp.Process]):
+    for process in processes:
+        process.join()
 
 def main(args):
     if len(args) != 2:
@@ -21,7 +38,14 @@ def main(args):
     directory = Path(args[1])
     if not(directory.is_dir()):
         raise ValueError("Incorrect Arguments: Directory does not exist")
+    dir_contents = listdir(directory)
+    pdf_files = [directory.joinpath(file) for file in dir_contents if Path(file).suffix == ".pdf"]
+    if not pdf_files:
+        raise ValueError("No PDF files found")
     create_dirs(directory)
+    processes = assign_processes(pdf_files)
+    join_processes(processes)
 
 if __name__ == "__main__":
+    argv.append("test")
     main(argv)
