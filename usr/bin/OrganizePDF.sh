b850e3d0236b698d145138540c50f92ce4629efe
Rami-CUD <20220001825@students.cud.ac.ae>
2024-11-20 08:42:05 +0400

    Restructured for debian packaging

---
+++ usr/bin/OrganizePDF.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+pdf_dir="."     
+report_dir="."
+while getopts 'd:r:' OPTION; do
+    case "${OPTION}" in 
+        d) 
+            pdf_dir="${OPTARG}"
+            ;;
+        r)
+            report_dir="${OPTARG}"
+            ;;
+        *) echo "Bad >:("
+            exit 1 ;;
+    esac
+done
+
+
+python organizer.py "$pdf_dir" "$report_dir"
+
